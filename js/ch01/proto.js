6afa3973bbc362248ebf5a95c540f3fa790c2058
Scott Davidson <scott@scott-davidson.com>
2013-07-01 06:06:50 -0700

    initial commit

---
+++ js/ch01/proto.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello from JavaScript!</title>
+</head>
+<body>
+    <script>
+        alert("Hello World!");
+        document.write("Log text ...");
+    </script>
+</body>
+</html>
